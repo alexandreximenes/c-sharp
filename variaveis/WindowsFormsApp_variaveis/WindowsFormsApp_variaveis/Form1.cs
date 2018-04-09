dd239ccea34879fd4bac81cebb375ccb76fd6c0d
alexandre ximenes <xyymenes@gmail.com>
2018-04-09 13:23:33 -0300

    testando progrmas com TDD

--- variaveis/WindowsFormsApp_variaveis/WindowsFormsApp_variaveis/Form1.cs
+++ variaveis/WindowsFormsApp_variaveis/WindowsFormsApp_variaveis/Form1.cs
@@ -7,22 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NUnit.Framework;
+
+/// <summary>
+/// V* Simulando teste TDD  
+/// </summary>
 
 namespace WindowsFormsApp_variaveis
 {
+    [TestFixture]
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
+        [Test]
         private void button1_Click(object sender, EventArgs e)
         {
-            int NumeroConta = 0001;
-            double Saldo = 1000.00;
-            double SaldoAtual = Saldo - 100.00;
-            MessageBox.Show("Meu saldo atual da conta " + NumeroConta +" é R$ " + SaldoAtual);
+            int numeroConta = 0001;
+            double saldo = 1000.00;
+            
+            //Somando com shortcut
+            saldo += 500.00;
+            double saldoAtual = saldo - 100.00;
+            
+            //Saldo esperado para teste
+            double saldoEsperado = 1400.00;
+            
+            //com exito
+            Assert.AreEqual(saldoEsperado, saldoAtual);
+            
+            //com falha
+            //Assert.AreEqual(1500, saldoAtual);
+            MessageBox.Show("Meu saldo atual da conta " + numeroConta +" é R$ " + saldoAtual);
         }
     }
 }
