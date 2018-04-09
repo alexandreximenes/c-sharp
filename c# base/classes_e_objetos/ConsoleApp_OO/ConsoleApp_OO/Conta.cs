f800eb5e321adbc5fda1771783a554d2e572c0ad
alexandre ximenes <xyymenes@gmail.com>
2018-04-09 16:03:17 -0300

    comecando com Orientacao a Objetos no c-sharp

---
+++ c# base/classes_e_objetos/ConsoleApp_OO/ConsoleApp_OO/Conta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_OO
+{
+    class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+    }
+}
---
+++ c# base/classes_e_objetos/ConsoleApp_OO/ConsoleApp_OO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp_OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conta\n");
+
+            Conta alexandre = new Conta();
+            alexandre.numero = 1;
+            alexandre.titular = "Alexandre";
+            alexandre.saldo = 10;
+
+            Conta arhtur = new Conta();
+            arhtur.numero = 1;
+            arhtur.titular = "Arthur";
+            arhtur.saldo = 100;
+
+
+            Console.WriteLine("Saldo do" + alexandre.titular + " é " + alexandre.saldo);
+            Console.WriteLine("Saldo do" + arhtur.titular + " é " + arhtur.saldo);
+
+            Console.ReadKey();
+        }
+    }
+}
