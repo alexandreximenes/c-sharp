519ba8377bab752447a425d4b1f65d9aae82131e
alexandre ximenes <xyymenes@gmail.com>
2018-04-09 18:40:32 -0300

    Trabalhando com encapsulamento

---
+++ c# base/classes_e_objetos/ConsoleApp_OO_Encapsulamento/ConsoleApp_OO_Encapsulamento/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_OO_Encapsulamento
+{
+    class Cliente
+    {
+        public String Titular { get; set; }
+        
+    }
+}
---
+++ c# base/classes_e_objetos/ConsoleApp_OO_Encapsulamento/ConsoleApp_OO_Encapsulamento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp_OO_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n************** Conta ************** \n\n");
+
+            //Cria os objetos
+            Conta cc = new Conta();
+
+            //Seta os valores
+            cc.Deposita(1000000010);
+
+            cc.Saca(5000);
+            cc.Saca(45000);
+            cc.Cliente = new Cliente();
+            cc.Cliente.Titular = "Alexandre";
+
+            Console.WriteLine("Saldo do " + cc.Cliente.Titular + " é " + cc.Saldo);
+            
+            Console.ReadKey();
+        }
+    }
+}
