58d9ca79bff1465536c35f01d01a164d98b8c71a
alexandre ximenes <xyymenes@gmail.com>
2018-04-09 14:56:30 -0300

    estrutura de controle

---
+++ Estrutura de controle/WindowsFormsApp_Estrutura de controle/WindowsFormsApp_Estrutura de controle/nomeNoSPC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Estrutura_de_controle
+{
+    public partial class nomeNoSPC : Form
+    {
+        public nomeNoSPC()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool nomeNoSPC = true;
+            int idade = 21;
+
+            if (idade < 18 || nomeNoSPC)
+            {
+                MessageBox.Show("Estamos analisando sua proposta");
+            }
+            else
+            {
+                MessageBox.Show("Você pode adquirir um cartão de crédito");
+            }
+        }
+    }
+}
---
+++ Estrutura de controle/WindowsFormsApp_Estrutura de controle/WindowsFormsApp_Estrutura de controle/saldo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp_Estrutura_de_controle
+{
+    public partial class saldo : Form
+    {
+        public saldo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Modo 1
+            double saldo = 100.0;
+            double valorSaque = 10.0;
+
+            bool realmentePodeSacar = (saldo >= valorSaque) && (valorSaque > 0);
+            if (realmentePodeSacar)
+            {
+                // código do saque
+                saldo -= valorSaque;
+                MessageBox.Show("Saldo realizado com sucesso");
+                MessageBox.Show("Seu novo saldo é: " + saldo);
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+
+            // Modo 2
+            double resultado = (saldo >= valorSaque) ? saldo -= valorSaque : 0;
+
+            MessageBox.Show("Resultado =="+resultado);
+        }
+    }
+}
