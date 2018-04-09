3d5ad9502138fe0d98c7d85f0d99abbfce9b4931
alexandre ximenes <xyymenes@gmail.com>
2018-04-09 20:07:29 -0300

    trabalhando com heranca adicionando totalizador de saldos

--- c# Orientado a objetos/Heranca/Heranca/Heranca/Program.cs
+++ c# Orientado a objetos/Heranca/Heranca/Heranca/Program.cs
@@ -8,11 +8,24 @@ namespace Heranca
         {
             Console.WriteLine("Hello World!");
 
+            //Instancia uma conta poupança
             Conta cp = new ContaPoupanca();
             cp.Deposita(100);
             cp.Saca(10);
 
+            //Instancia outra uma conta poupança
+            Conta cp2 = new ContaPoupanca();
+            cp2.Deposita(500);
+            cp2.Saca(50);
+
+            //totalizado o saldo das contas
+            TotalizadorDeSaldos totalizadorDeSaldos = new TotalizadorDeSaldos();
+            totalizadorDeSaldos.adiciona(cp);
+            totalizadorDeSaldos.adiciona(cp2);
+
             Console.WriteLine("saldo da conta poupança é  "+ cp.Saldo);
+            Console.WriteLine("saldo da conta poupança é  " + cp2.Saldo);
+            Console.WriteLine("saldo total das contas  " + totalizadorDeSaldos.saldoTotal());
             Console.ReadKey();
         }
     }
---
+++ c# Orientado a objetos/Heranca/Heranca/Heranca/TotalizadorDeSaldos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca
+{
+    class TotalizadorDeSaldos
+    {
+        private double total = 0;
+        public void adiciona(Conta conta) {
+            total += conta.Saldo;
+        }
+
+        public double saldoTotal() {
+            return total;
+        }
+    }
+}
