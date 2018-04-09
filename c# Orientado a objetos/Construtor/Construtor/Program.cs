b825c9bb6c282fb4e3edd6a23343c64619313757
alexandre ximenes <xyymenes@gmail.com>
2018-04-09 19:19:46 -0300

    trabalhando com construtor

---
+++ c# Orientado a objetos/Construtor/Construtor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
