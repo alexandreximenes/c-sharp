10668fafb9e4ea67850ab88bc83bbc32bca6f3ad
alexandre ximenes <xyymenes@gmail.com>
2018-04-09 21:44:45 -0300

    tipagem implicita nas variaveis

---
+++ c# Orientado a objetos/TipagemImplicita/app/app/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app
+{
+    class Cliente
+    {
+        public String Nome { private get; set; }
+        public int Idade { get; set; }
+        public bool EEmancipado { get; set; }
+        public string Cpf { get; set; }
+        public const int IDADE_MINIMA = 18;
+
+        public Cliente(String nome, int idade, bool EEmancipado, string cpf)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.EEmancipado = EEmancipado;
+            this.Cpf = cpf;
+        }
+
+
+        public bool PodeAbrirConta() {
+            //Tipagem implicita -> var
+            var maiorDeIdade = (this.Idade >= IDADE_MINIMA);
+            var EEmancipado = (this.EEmancipado);
+            // verifica se o valor NÃO é nulo ou vazio
+            var temCpf = !string.IsNullOrEmpty(this.Cpf);
+
+            return (maiorDeIdade || EEmancipado) && temCpf;
+        }
+
+    }
+}
---
+++ c# Orientado a objetos/TipagemImplicita/app/app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nâo funciona, lado direto tem que ser algo concreto para ser implicito
+            //var nula = null;
+
+            var alexandre = new Cliente("Alexandre", 18, true, "123456-58");
+
+            string podeAbrirConta = (alexandre.PodeAbrirConta() ? "Sim" : "Não");
+
+            Console.WriteLine("Cliente pode abrir conta :" + (podeAbrirConta));
+            Console.ReadKey();
+        }
+    }
+}
