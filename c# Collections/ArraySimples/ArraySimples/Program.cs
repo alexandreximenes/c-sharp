569e3180c1dc0e96574bfa81597b8d6ac17108ef
alexandre ximenes <xyymenes@gmail.com>
2018-04-11 12:07:52 -0300

    introducao ao C# com array simples

---
+++ c# Collections/ArraySimples/ArraySimples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando um array implicitamente;
+
+            /*string posicao0 = "introducao á collections no C#";
+            string posicao1 = "trabalhando com arrays simples";
+            string posicao2 = "trabalhando com array explicitamente";
+
+            string[] meuArray = new string[]
+            {
+                    posicao0,
+                    posicao1,
+                    posicao2,
+            };*/
+
+            // Criando um array explicitamente;
+
+            string[] meuArray = new string[3];
+            meuArray[0] = "introducao á collections no C#";
+            meuArray[1] = "trabalhando com arrays simples";
+            meuArray[2] = "trabalhando com array explicitamente";
+
+            imprimirArray(meuArray);
+            Console.WriteLine(meuArray[0]);
+            Console.ReadKey();
+        }
+
+        private static void imprimirArray(string[] meuArray)
+        {
+            foreach (var array in meuArray)
+            {
+                Console.WriteLine(array);
+            }
+        }
+    }
+}
