58c4b12f9962dae384b2f9e9bf3b1f199bea4b87
alexandre ximenes <xyymenes@gmail.com>
2018-04-11 10:33:51 -0300

    aprendendo atalhos uteis para aumentar a velocidade do desenvolvimento

---
+++ c# Collections/ListComObjetos/ListComObjetos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListComObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class treinamento {
+        private string titulo;
+        private int duracao;
+
+        public treinamento(string titulo, int duracao)
+        {
+            Titulo = titulo;
+            Duracao = duracao;
+
+        }
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public int Duracao { get => duracao; set => duracao = value; }
+    }
+}
