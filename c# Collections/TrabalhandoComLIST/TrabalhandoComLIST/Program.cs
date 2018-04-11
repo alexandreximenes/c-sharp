2f4a5e9844c1fa5256829fd1b05a15293f0e2c94
alexandre ximenes <xyymenes@gmail.com>
2018-04-11 10:25:03 -0300

    trabalhando com mais metodos auxiliares de List

--- c# Collections/TrabalhandoComLIST/TrabalhandoComLIST/Program.cs
+++ c# Collections/TrabalhandoComLIST/TrabalhandoComLIST/Program.cs
@@ -27,7 +27,7 @@ namespace TrabalhandoComLIST
             treinamento.Add("adicionando um novo item na lista");
 
             //tamanho da lista
-            Console.WriteLine( "Tamanho da lista : "+ treinamento.Count() );
+            Console.WriteLine("Tamanho da lista : " + treinamento.Count());
 
             //imprimindo a primeira posicao do array
             Console.WriteLine("primeira posicao da lista : " + treinamento.First());
@@ -36,39 +36,72 @@ namespace TrabalhandoComLIST
             Console.WriteLine("ultima posicao da lista : " + treinamento.Last());
 
             //Verificando se existe um item no array
-            Console.WriteLine("Verificando se existe um item no array :  " +treinamento.Contains(intro));
+            Console.WriteLine("Verificando se existe um item no array :  " + treinamento.Contains(intro));
 
 
             try
             {
                 //trabalhando com expressoes lambda
                 //procura a primeira ocorrencia dentro do array com a palavra 'trabalhando'
-                Console.WriteLine("A PRIMEIRA ocorrencia da palavra 'trabalhando' é \t"
+                Console.WriteLine("A PRIMEIRA ocorrencia da palavra 'trabalhando' é :  "
                                     + treinamento.First(t => t.Contains("trabalhando")));
 
                 //procura a ultima ocorrencia dentro do array com a palavra 'trabalhando'
-                Console.WriteLine("A ULTIMA ocorrencia da palavra 'trabalhando' é \t"
+                Console.WriteLine("A ULTIMA ocorrencia da palavra 'trabalhando' é :  "
                                     + treinamento.Last(t => t.Contains("trabalhando")));
             }
             catch (Exception) { /*tratamento da exceção */}
 
             //procura a primeira ocorrencia ou DEFAULT dentro do array com a palavra 'trabalhando'
-            Console.WriteLine("A PRIMEIRA OU DEFAULT (VAZIO) ocorrencia da palavra 'trabalhando' é \t"
+            Console.WriteLine("A PRIMEIRA OU DEFAULT (VAZIO) ocorrencia da palavra 'trabalhando' é : "
                                 + treinamento.FirstOrDefault(t => t.Contains("textoNaoEncontrado")));
 
             //Invertendo a lista
             treinamento.Reverse();
 
-            //Removendo itens da lista
+            //Removendo o ultimo item da lista
+            treinamento.RemoveAt(treinamento.Count() - 1);
+
+            //tamanho da lista
+            Console.WriteLine("\nTamanho da lista : " + treinamento.Count());
+
+            //Ordenando a lista
+            treinamento.Sort();
 
             //Trabalhando com Lambdas
             treinamento.ForEach(t => Console.WriteLine(t));
+
+            //Fazendo uma copia
+            /*
+             * Copiando elementos, passando tamanho do elemento, 
+             * pegando o penultimo elemento
+             * e pegar 2 elementos 
+             */
+            var tamanhoTreinamento = treinamento.Count();
+            List<string> copia = treinamento.GetRange(tamanhoTreinamento - 2, 2);
+
+            imprimirComLambda("copia", copia);
+
+            //Clonando uma lista
+            List<string> clone = new List<string>(treinamento);
+            imprimirComLambda("clone", clone);
+
+            //Removendo um intervalo especifico da lista
+            clone.RemoveRange(clone.Count - 2, 2);
+            imprimirComLambda("clone", clone);
 
             //imprimir(treinamento);
 
             Console.ReadKey();
         }
 
+        private static void imprimirComLambda(string nomeLista, List<string> array)
+        {
+
+            //imprimindo copia
+            Console.WriteLine("\nimprimindo "+nomeLista+  " do array");
+            array.ForEach(c => Console.WriteLine(c));
+        }
 
         private static void imprimirForTradicional(List<string> treinamento)
         {
