fb2ba8c53602962aa7e3df32ec3bb49dde7ae336
alexandre ximenes <xyymenes@gmail.com>
2018-04-11 22:29:51 -0300

    alterando pasta do treinamento

---
+++ c# base/Topicos especiais/Trabalhando com Equals e HashCode/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroExcecoes
+{
+    class Cliente
+    {
+        public int Numero { get; }
+        public string Nome { get; }
+
+        public Cliente(int numero, string nome)
+        {
+            try
+            {
+                this.Numero = numero;
+                this.Nome = nome;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Não foi possivel capturar os campos do cliente");
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            Cliente outro = (Cliente)obj;
+
+            return this.Nome.Equals(outro.Nome);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", conta numero : " + Numero; 
+        }
+
+    }
+}
---
+++ c# base/Topicos especiais/Trabalhando com Equals e HashCode/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroExcecoes
+{
+    public partial class FormConta : Form
+    {
+        public FormConta()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+
+            Cliente cliente = new Cliente(Convert.ToInt32(tbNumero.Text), tbTitular.Text);
+            Conta conta = new Conta(cliente, Convert.ToDouble(tbSaldo.Text) );
+
+            Cliente cliente2 = new Cliente(Convert.ToInt32(tbNumero.Text), tbTitular.Text);
+
+            MessageBox.Show("Equals = "+ cliente.Equals(cliente2) );
+            MessageBox.Show("ToString = " + cliente.ToString() );
+
+            try {
+                conta.Saca(Convert.ToDouble(tbValorSaque.Text));
+                MessageBox.Show("Dinheiro liberado");
+                tbSaldo.Text = Convert.ToString(conta.Saldo);
+            }
+            catch (Exception) {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+
+    }
+}
