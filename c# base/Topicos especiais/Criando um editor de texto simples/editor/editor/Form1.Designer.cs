c14e6440579c30806633b519119ea1998ebbc131
alexandre ximenes <xyymenes@gmail.com>
2018-04-12 12:56:47 -0300

    trabalhando com arquivos, escrita e leitura

---
+++ c# base/Topicos especiais/Criando um editor de texto simples/editor/editor/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.texto = new System.Windows.Forms.TextBox();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // texto
+            // 
+            this.texto.Location = new System.Drawing.Point(-1, 0);
+            this.texto.Multiline = true;
+            this.texto.Name = "texto";
+            this.texto.Size = new System.Drawing.Size(383, 270);
+            this.texto.TabIndex = 0;
+            this.texto.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(271, 276);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(98, 23);
+            this.btSalvar.TabIndex = 1;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 309);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.texto);
+            this.Name = "Form1";
+            this.Text = "Editor de texto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox texto;
+        private System.Windows.Forms.Button btSalvar;
+    }
+}
+
---
+++ c# base/Topicos especiais/Criando um editor de texto simples/editor/editor/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("entrada.txt"))
+            {
+                Stream entrada = null;
+                StreamReader leitor = null;
+                try
+                {
+                    entrada = File.Open("entrada.txt", FileMode.Open);
+                    leitor = new StreamReader(entrada);
+                    string linha = leitor.ReadLine();
+
+                    while (linha != null)
+                    {
+                        texto.Text += linha;
+                        linha = leitor.ReadLine();
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Problemas ao ler o arquivo!");
+                }
+                finally { 
+                    leitor.Close();
+                    entrada.Close();
+                }
+            }
+            else {
+                MessageBox.Show("arquivo inexistente!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream saida = null;
+            StreamWriter escritor = null;
+            try
+            {
+                saida = File.Open("entrada.txt", FileMode.Create);
+                escritor = new StreamWriter(saida);
+                escritor.Write(texto.Text);
+
+                MessageBox.Show("Arquivo gravado com sucesso");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro ao gravar arquivo!");
+            }
+            finally {
+                escritor.Close();
+                saida.Close();
+            }
+
+        }
+    }
+}
