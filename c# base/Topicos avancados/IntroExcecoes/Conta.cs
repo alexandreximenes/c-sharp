6d4cc6edc9d81a9ae14b82941050601b62187765
alexandre ximenes <xyymenes@gmail.com>
2018-04-11 21:48:15 -0300

    comecando com topicos avancados

---
+++ c# base/Topicos avancados/IntroExcecoes/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroExcecoes
+{
+    class Conta
+    {
+        public Cliente Cliente { get; }
+        public double Saldo { get; protected set; }
+        private double valor = 0;
+        public Conta(Cliente cliente, double saldo)
+        {
+            try {
+                Cliente = cliente;
+                Saldo = saldo;
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("Não foi possivel capturar os dados da conta");
+            }
+        }
+
+        public bool Saca(double valor)
+        {
+            try
+            {
+                this.valor = valor;
+            }
+            catch (Exception e) {
+                Console.WriteLine("Não conseguir converter o valor do saque");
+            }
+            if (valor < 0) {
+                throw new ArgumentException();
+            }
+            if (valor <= this.Saldo)
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            else
+            {
+                throw new SaldoInsuficienteException();
+            }
+        }
+    }
+}
---
+++ c# base/Topicos avancados/IntroExcecoes/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroExcecoes
+{
+    public partial class FormConta : Form
+    {
+        public FormConta()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+
+            Cliente cliente = new Cliente(Convert.ToInt32(tbNumero.Text), tbTitular.Text);
+            Conta conta = new Conta(cliente, Convert.ToDouble(tbSaldo.Text) );
+
+            try {
+                conta.Saca(Convert.ToDouble(tbValorSaque.Text));
+                MessageBox.Show("Dinheiro liberado");
+                tbSaldo.Text = Convert.ToString(conta.Saldo);
+            }
+            catch (Exception) {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+
+    }
+}
